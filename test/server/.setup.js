b07ad950b40fc3d8bcadcdbc66bcaf80f378dd09
John Oksasoglu <john.oksasoglu@gmail.com>
2016-10-20 11:36:07 -0700

    Update server test command to find nested tests

--- test/server/.setup.js
+++ test/server/.setup.js
@@ -1,2 +1,5 @@
+// Mocha will make these available as globals to all tests
+// which prevents us from having to manually require common modules
+// within each test file
 global.expect = require('chai').expect;
 global.request = require('request');
