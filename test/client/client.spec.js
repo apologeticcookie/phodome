fa04149a11b6e027af1cb4eaca6dbb9ed5362c2c
Tony K Tan <tankwantiong@gmail.com>
2016-10-20 16:24:29 -0700

    Attempt fixing bug present in test/client.client.spec.js. needs more work

--- test/client/client.spec.js
+++ test/client/client.spec.js
@@ -8,3 +8,11 @@ describe('<VRScene />', () => {
     expect(wrapper.find(Scene)).to.have.length(1);
   });
 });
+
+// bug present: 
+// /Users/tonyktan/Desktop/Hack Reactor/apologeticcookie/node_modules/aframe/dist/aframe.js:56669
+//     if (WebVRConfig.ENABLE_DEPRECATED_API) {
+//         ^
+
+// ReferenceError: WebVRConfig is not defined
+//     at new WebVRPolyfill (/Users/tonyktan/Desktop/Hack Reactor/apologeticcookie/node_modules/aframe/dist/node_modules/webvr-polyfill/build/webvr-polyfill.js:6517:1)
