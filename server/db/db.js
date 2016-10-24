b59f2391876de1735407385bfaf8b27ca4444ba5
Tony K Tan <tankwantiong@gmail.com>
2016-10-24 11:17:25 -0700

    Modifying database setup and instructions

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,12 @@
 // Database set up
 // Ref: http://docs.sequelizejs.com/en/latest/docs/getting-started/
 // Ref on localhost URI: http://stackoverflow.com/questions/3582552/postgres-connection-url
+
 // Notes to set up local database:
 // a) Download Postgres App from http://postgresapp.com/
 // b) Start local database from the Desktop app
 // c) start server using 'node app.js'
+
 var Sequelize = require('sequelize');
 var db = new Sequelize(process.env.DATABASE_URL || 'postgresql://localhost');
 
