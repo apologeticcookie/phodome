f1492b3be0d864023c0e7cdbedd1ce2807efe8c3
John Oksasoglu <john.oksasoglu@gmail.com>
2016-10-24 14:03:27 -0700

    (cleanup) Remove unnecessary ReactDOM conditional logic
    Formerly I thought we had to do some conditional logic on
    what node ReactDOM would render to because of karma, but it
    turns out we don't need to.

--- client/src/app.js
+++ client/src/app.js
@@ -2,15 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PhodomeScene from './components/PhodomeScene';
 
-// The only purpose of this target variable is so that when running the client
-// tests inside Chrome with Karma, where OUR index.html is not loaded, React
-// won't try to render to a nonexistent DOM element .scene-container and will
-// instead just render on the document body (which is guaranteed to exist on
-// any given page, I think)
-let target = document.querySelector('.scene-container');
-
-if (!target) {
-  target = document.querySelector('body');
-}
-
-ReactDOM.render(<PhodomeScene/>, target);
+ReactDOM.render(<PhodomeScene />, document.querySelector('.scene-container'));
