51ca978d87eb0478838e3376db878f22982069aa
Tony K Tan <tankwantiong@gmail.com>
2016-10-24 11:31:20 -0700

    Amend code to use database name 'phodome' in db.js and .travis.yml

--- server/db/db.js
+++ server/db/db.js
@@ -4,10 +4,11 @@
 
 // Notes to set up local database:
 // a) Download Postgres App from http://postgresapp.com/
-// b) Start local database from the Desktop app
+// b) Start local database from the Postgres Desktop app
+// c) In the PostgresSQL shell, enter 'CREATE DATABASE phodome;'
 // c) start server using 'node app.js'
 
 var Sequelize = require('sequelize');
-var db = new Sequelize(process.env.DATABASE_URL || 'postgresql://localhost');
+var db = new Sequelize(process.env.DATABASE_URL || 'postgresql://localhost/phodome');
 
 module.exports = db;
