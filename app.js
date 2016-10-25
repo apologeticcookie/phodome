c8293393cc75362b0a1148fa6a9fc604f0a4b073
riyazgithub <sunez.riyaz@gmail.com>
2016-10-25 11:41:06 -0700

    Implement static server to serve images

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ var express = require('express');
 var app = express();
 var db = require('./server/db/db');
 var Photo = require('./server/db/models/Photo');
+var morgan = require('morgan'); // middleware for logging request details
+var bodyParser = require('body-parser'); // midleware supports unicode encoding of the body
+var requestHandler = require('./server/helpers/requestHandler').router;
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(allowCrossDomain);
+app.use('/api', requestHandler);
 
 app.use(express.static(__dirname + '/client'));
 
@@ -9,7 +27,8 @@ app.listen(process.env.PORT || 9999, function() {
   console.log(`listening on port ${process.env.PORT || 9999}`);
 });
 
-// Sample Sequelize code for integrating into api calls
+
+module.exports = app;
 
 // Creates a table named 'photos'
 // force: true will drop the table if it already exists
