c5f168e781bba6b2fded5cfc99f54ca508bcf38a
Tony K Tan <tankwantiong@gmail.com>
2016-10-26 13:15:02 -0700

    (feat) Work on homepage buttons

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,16 +2,25 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const buttonStyle = {
+  margin: 12
+};
 
 const HomePage = props => (
   <MuiThemeProvider>
     <div>
       <AppBar
-        iconElementRight={<IconButton iconClassName="muidocs-icon-custom-github" />}
+        iconElementRight={
+          <IconButton 
+            iconClassName='muidocs-icon-custom-github' 
+            href='https://github.com/apologeticcookie/apologeticcookie' />}
       />
-      <header>Phodome</header>
-      <IconButton iconClassName="muidocs-icon-custom-github" />
-      <a onClick={props.toggleDemo}>My Photos</a> 
+      <h1>Phodome</h1>
+      <p>See your photos around you, truly.</p>
+      <RaisedButton label="Demo" style={buttonStyle} />
     </div>
   </MuiThemeProvider>
 );
