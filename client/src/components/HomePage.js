2c259a2e16892ca68954ac60a156240ed9269377
Tony K Tan <tankwantiong@gmail.com>
2016-10-26 12:59:26 -0700

    (feat) Attempt getting github icon to show up

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
 
 const HomePage = props => (
-  <div>
-    <header class="phodome">Phodome</header>
-    <a onClick={props.toggleDemo}>My Photos</a>
-  </div>
+  <MuiThemeProvider>
+    <div>
+      <AppBar
+        iconElementRight={<IconButton iconClassName="muidocs-icon-custom-github" />}
+      />
+      <header>Phodome</header>
+      <IconButton iconClassName="muidocs-icon-custom-github" />
+      <a onClick={props.toggleDemo}>My Photos</a> 
+    </div>
+  </MuiThemeProvider>
 );
 
 export default HomePage
