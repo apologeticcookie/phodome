cb0742f918cc9e0c44570347a5d4a8aa526ae15d
John Oksasoglu <john.oksasoglu@gmail.com>
2016-10-31 11:15:27 -0700

    (fix) Fix image positions shifting after upload

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,6 +1,18 @@
 import { Entity } from 'aframe-react';
 import React from 'react';
 
+/*
+  The 'look-at' property of each image is set to point to our <Camera> that is
+  rendered inside <PhodomeScene>
+  The <Camera> component is given an id of 'camera' (check out Camera.js) so that
+  it can easily be referred to
+  Formerly, the 'look-at' property was set to the string '0 0 0' to always point
+  at the center of the dome, but for some reason, after uploading new photos to
+  the dome, thereby triggering a re-fetching and re-rendering of the dome, photo
+  positions would be sometimes wildly off and NOT actually facing the origin.
+  Setting each <Image> to instead simply always point to the camera seems to fix
+  this issue.
+*/
 const Image = props => (
   <Entity
     geometry={{
@@ -10,7 +22,7 @@ const Image = props => (
       depth: 0
     }}
     position={props.position}
-    look-at='0 0 0'
+    look-at="#camera"
     material={{
       src: `url(${props.src})`
     }}
