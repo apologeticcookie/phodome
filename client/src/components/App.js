c7bf8d76f621faaa2fa8c285375bef658c061c2c
John Oksasoglu <john.oksasoglu@gmail.com>
2016-10-31 10:55:50 -0700

    (fix) Maintain Plotly dome even on view switches

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,9 +19,30 @@ class App extends React.Component {
   }
 
   render() {
-    return this.state.showHomePage ?
-    (<HomePage toggleDemo={this.toggleDemo}/>) :
-    (<Container toggleDemo={this.toggleDemo}/>);
+    // If home page should NOT be shown, simply hide the <HomePage> component
+    // rather than remove it completely. Because the Plotly visualization on
+    // <HomePage> is run through a <script> tag inside index.html, and is
+    // therefore done outside the scope of React, we want to maintain the
+    // existence of <HomePage> in the DOM; otherwise, we would need to re-trigger
+    // the dataViz.js script every time we switched from <Container> to <HomePage>
+    // Without this fix, the Plotly dome visualization would be absent when
+    // switching from <Container> to <HomePage>
+    let style = this.state.showHomePage ?
+      {} :
+      { display: 'none' };
+
+    return (
+      <div>
+        <div style={style} className="llama">
+          <HomePage toggleDemo={this.toggleDemo}/>
+        </div>
+        {
+          this.state.showHomePage ?
+            null :
+            <Container toggleDemo={this.toggleDemo}/>
+        }
+      </div>
+    );
   }
 }
 
