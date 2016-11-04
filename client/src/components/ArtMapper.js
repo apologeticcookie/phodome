c6258c14723f765af8201a8843b7b50c7bdf471d
El_Gato <albertomadueno@gmail.com>
2016-11-04 11:35:15 -0700

    created VR room that dynamically renders images and lighting

---
+++ client/src/components/ArtMapper.js
@@ -0,0 +1,48 @@
+import 'aframe';
+import 'aframe-animation-component';
+import 'aframe-text-component';
+import { Entity } from 'aframe-react';
+import React from 'react';
+
+import ArtData from '../../../client/assets/dummy-data/dummy';
+import Art from './Bart';
+
+const ArtMapper = props => {
+  const z = 9;
+  const y = 3;
+  let x = 0;
+  let lightX = 0;
+  let count = 0;
+
+  return (
+    <Entity>
+      {
+        props.images.map((imageUrl, index) => {
+          // The negative flips all items from behind the camera to in front
+          // 8 12 6 light position
+          x += 8;
+          lightX +=12; 
+          count++;
+          return (
+            <Entity>
+              <Art
+              key={imageUrl}
+              src={imageUrl}
+              position={`${x} ${3} ${9}`}/>
+
+              <Entity 
+              light="type: spot; angle: 30"
+              intensity="1"
+              look-at="" 
+              rotation="-60 180 0" 
+              position={`${x} ${12} ${8}`}/>
+            </Entity>
+          );
+        })
+      }
+    </Entity>
+  );
+};
+
+
+export default ArtMapper;
